de9cfbd5335363ba28b654a73f559407bd5be453
Remousses <remy91490@hotmail.fr>
2020-09-23 23:14:31 +0200

    test toMakeDeposit and getTransactionHistory - OK

--- Front-End/src/app/home/home.component.ts
+++ Front-End/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Operation } from '../models/operation';
+import { ITransaction } from '../models/transaction';
+import { IUser } from '../models/user';
+import { TransactionService } from '../services/transaction/transaction.service';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'kata-home',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private transactionService: TransactionService, private userService: UserService) { }
 
   ngOnInit(): void {
+    const currentUser: IUser = this.userService.getCurrentUser()
+    const transaction: ITransaction = {
+      idUser: currentUser.id,
+      operation: Operation.DEPOSIT,
+      wording: 'Premier test de versement',
+      date: new Date(),
+      amount: 200
+    }
+    
+    this.transactionService.toMakeDeposit(transaction).subscribe()
+    this.transactionService.getTransactionHistory().subscribe()
   }
 
 }
