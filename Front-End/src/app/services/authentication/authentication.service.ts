13b12b987d124cfe599cd221e75a867916efa16c
Remousses <remy91490@hotmail.fr>
2020-09-23 23:09:17 +0200

    add feature login and logout

--- Front-End/src/app/services/authentication/authentication.service.ts
+++ Front-End/src/app/services/authentication/authentication.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
-import { User } from '../../models/user';
-import { shared } from '../../shared/index';
+import { tap, catchError } from 'rxjs/operators'
+import { IUser } from '../../models/user'
+import { shared } from '../../shared/index'
 import { HttpHandleError } from '../../shared/http-handle-error'
+import { Router } from '@angular/router';
+import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +14,27 @@ import { HttpHandleError } from '../../shared/http-handle-error'
 export class AuthenticationService {
   private error = new HttpHandleError()
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private router: Router,
+              private userService: UserService) { }
 
-  login(user: User): Observable<User> {
+  login(user: IUser): Observable<IUser> {
     console.log('Connection');
     
-    return this.http.post(shared.apiUrl + "login", user).pipe(
-      tap((user: User) => console.log("User :", user)),
-      catchError(this.error.handleError<User>('authentication'))
+    return this.http.post<IUser>(shared.apiUrl + "login", user).pipe(
+      tap((user: IUser) => console.log("User :", user)),
+      catchError(this.error.handleError<IUser>('authentication'))
     )
+  }
+
+  logout(){
+    this.http.get(shared.apiUrl + "logout").pipe(
+      catchError(this.error.handleError<IUser>('authentication'))
+    )
+  }
+
+  logoutComplete(){
+    this.router.navigate(['login'])
+    this.userService.clearStorage();
   }
 }
