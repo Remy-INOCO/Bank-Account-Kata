bdacbcc061148cc97f68a7fd8e4af90e693ba0f1
Remousses <remy91490@hotmail.fr>
2020-09-23 22:59:24 +0200

    caching user and add logout feature

--- Back-End/src/main/java/com/inoco/kata/controller/AutenticationController.java
+++ Back-End/src/main/java/com/inoco/kata/controller/AutenticationController.java
@@ -4,39 +4,58 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inoco.kata.entity.User;
 import com.inoco.kata.repository.UserRepository;
+import com.inoco.kata.session.UserSession;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class AutenticationController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AutenticationController.class);
 
-	private final UserRepository repository;
+	@Autowired
+	private final UserRepository userRepository;
 
-	public AutenticationController(final UserRepository repository) {
-		this.repository = repository;
+	@Autowired
+	private final UserSession userSession;
+
+	@Autowired
+	public AutenticationController(final UserRepository userRepository, final UserSession userSession) {
+		this.userRepository = userRepository;
+		this.userSession = userSession;
 	}
 
 	@PostMapping("/login")
-	public User getAuthentication(@RequestBody final User userAuth) throws Exception {
+	public User login(@RequestBody final User userAuth) throws Exception {
 		System.out.println("USER : " + userAuth);
 
-		final Optional<User> optionalUser = this.repository.findAll().stream()
+		final Optional<User> optionalUser = this.userRepository.findAll().stream()
 				.filter(user -> this.checkUser(user, userAuth)).findFirst();
 
 		if (optionalUser.isPresent()) {
 			final User currentUser = optionalUser.get();
 			currentUser.setPassword(null);
-			return currentUser;
+			this.userSession.setCurrentUser(currentUser);
+			return this.getUserSession();
 		}
 
 		return null;
+	}
+
+	@GetMapping("/logout")
+	public void logout() {
+		this.userSession.setCurrentUser(null);
+	}
+
+	private User getUserSession() {
+		return this.userSession.getCurrentUser();
 	}
 
 	private boolean checkUser(final User user, final User userAuth) {
