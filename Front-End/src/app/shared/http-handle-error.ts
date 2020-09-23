1fc405250dbedecfc6cb6a82d75c2ef579cf9308
Remousses <remy91490@hotmail.fr>
2020-09-23 23:08:46 +0200

    stringify error http

--- Front-End/src/app/shared/http-handle-error.ts
+++ Front-End/src/app/shared/http-handle-error.ts
@@ -3,7 +3,7 @@ import { Observable, of } from 'rxjs';
 export class HttpHandleError {
     handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
-          console.error(operation + " failed : " + error); // log to console instead
+          console.error(operation + " failed : " + JSON.stringify(error)); // log to console instead
     
           return of(result as T);
         }
