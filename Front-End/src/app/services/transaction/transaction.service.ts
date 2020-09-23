797b34180d024c420865848d9d3f8d0ed4c3554b
Remousses <remy91490@hotmail.fr>
2020-09-23 23:12:51 +0200

    add feature getTransactionHistory and toMakeDeposit

--- Front-End/src/app/services/transaction/transaction.service.ts
+++ Front-End/src/app/services/transaction/transaction.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators'
+import { ITransaction } from 'src/app/models/transaction';
+import { HttpHandleError } from 'src/app/shared/http-handle-error';
+import { shared } from '../../shared/index'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
+  private transactionUrl = 'transactions/'
+  private error = new HttpHandleError()
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getTransactionHistory(): Observable<ITransaction[]> {
+    return this.httpClient.get<ITransaction[]>(shared.apiUrl + this.transactionUrl +  "history").pipe(
+      tap(transactions => console.log(transactions)),
+      catchError(this.error.handleError<ITransaction[]>('getTransactionHistory'))
+    )
+  }
+
+  toMakeDeposit(transaction: ITransaction): Observable<ITransaction> {
+    return this.httpClient.put<ITransaction>(shared.apiUrl + this.transactionUrl +  "deposit", transaction).pipe(
+      tap(result => console.log(result)),
+      catchError(this.error.handleError<ITransaction>('toMakeDeposit'))
+    )
+  }
 }
