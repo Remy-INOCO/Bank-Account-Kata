17371a50eafb04e5fe57af8bbb3d1c8619a6b2cb
Remousses <remy91490@hotmail.fr>
2020-10-02 22:42:59 +0200

    adding springfox-swagger dependency to add dynamically the wagger

--- Back-End/src/main/java/com/inoco/kata/api/configuration/KataApplication.java
+++ Back-End/src/main/java/com/inoco/kata/api/configuration/KataApplication.java
@@ -8,10 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EntityScan("com.inoco.kata.api.model")
 @EnableJpaRepositories("com.inoco.kata.api.repository")
 @ComponentScan(basePackages = { "com.inoco.kata.api" })
+@EnableSwagger2
 public class KataApplication {
 
 	public static void main(final String[] args) {
@@ -21,5 +30,17 @@ public class KataApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public Docket kataBankAccountApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.inoco.kata.api")).build().apiInfo(this.apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Swagger Bank account")
+				.description("This is an API to get information about a bank account")
+				.contact(new Contact("Argentin Rémy", "", "remy.argentin@inoco.fr")).build();
 	}
 }
